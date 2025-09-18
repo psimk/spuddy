bcca6f8d30e567daa799b77b870392812697c768
Paul <dev@psimk.dev>
2025-09-18 21:45:34 +0300

    setup `Geist` font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Geist",
+        cssVariable: "--font-geist",
+      },
+    ],
+  },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
+
