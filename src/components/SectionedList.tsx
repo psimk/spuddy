8402f7d2e1e56305caf122c3b8d17887acd11441
Paul <dev@psimk.dev>
2025-09-20 17:05:26 +0300

    add initial sectioned list demo

---
+++ src/components/SectionedList.tsx
@@ -0,0 +1,70 @@
+import { DragDropProvider, DragOverlay } from "@dnd-kit/react";
+import { AnimatePresence } from "motion/react";
+
+import dragDropManager from "../singletons/drag-drop-manager";
+
+import SortableListItem from "./SortableListItem";
+import DragOverlayListItem from "./DragOverlayListItem";
+
+type Product = {
+  id: string;
+  emoji: string;
+  categoryId: number;
+  variants: { en: ReadonlyArray<string>; lt: ReadonlyArray<string> };
+};
+
+type Category = {
+  id: string;
+  emoji: string;
+  title: { en: string; lt: string };
+};
+
+type Props = { categories: Array<Category>; products: Array<Product> };
+
+export default function SectionedList({ categories, products }: Props) {
+  return (
+    <DragDropProvider manager={dragDropManager}>
+      <ul className="flex flex-col gap-6">
+        {categories
+          .filter(
+            (category) =>
+              products.filter(
+                (product) => String(product.categoryId) === category.id,
+              ).length > 0,
+          )
+          .map((category) => {
+            const categoryProducts = products.filter(
+              (product) => String(product.categoryId) === category.id,
+            );
+            return (
+              <section key={category.id}>
+                <h2 className="text-md mb-3 text-center">
+                  ------ {category.title.en} ------
+                </h2>
+                <ul className="flex flex-col gap-3">
+                  {categoryProducts.map((product, index) => (
+                    <SortableListItem
+                      key={product.id}
+                      id={product.id}
+                      index={index}
+                      defaultValue={product.variants.en[0]}
+                    />
+                  ))}
+                </ul>
+              </section>
+            );
+          })}
+      </ul>
+
+      <AnimatePresence>
+        <DragOverlay>
+          {(source) => (
+            <DragOverlayListItem
+              {...products.find((p) => p.id === source.id)!}
+            />
+          )}
+        </DragOverlay>
+      </AnimatePresence>
+    </DragDropProvider>
+  );
+}
