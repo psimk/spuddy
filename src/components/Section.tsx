23a9aa0af079375b4afd0a9634af5c71a2d6e4c6
Paul <dev@psimk.dev>
2025-09-23 12:04:17 +0300

    implement collapsing

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,3 +1,4 @@
+import { Grip } from "lucide-react";
 import { forwardRef, type PropsWithChildren, type Ref } from "react";
 
 type Props = { title: string; handleRef?: Ref<HTMLElement> };
@@ -7,11 +8,20 @@ export default forwardRef(function Section(
   ref: Ref<HTMLHeadingElement>,
 ) {
   return (
-    <section ref={ref}>
-      <h2 ref={handleRef} className="text-md mb-3 text-center">
-        ------ {title} ------
+    <section className="collapse" ref={ref}>
+      <input type="checkbox" defaultChecked className="peer" />
+      <h2 className="collapse-title border-base-content/20 relative rounded-lg border p-2 text-center transition-all peer-checked:border-0 peer-checked:[&_span]:opacity-0">
+        {title}
+
+        <span
+          ref={handleRef}
+          className="absolute top-0 right-2 z-10 grid h-full cursor-move place-items-center opacity-100 transition-opacity"
+        >
+          <Grip />
+        </span>
       </h2>
-      {children}
+
+      <div className="collapse-content !p-0 !pt-2">{children}</div>
     </section>
   );
 });
