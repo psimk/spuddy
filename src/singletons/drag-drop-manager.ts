febe8e7daa6cf3c67a2136a033b036abc1021439
Paul <dev@psimk.dev>
2025-09-23 08:10:06 +0300

    add support for drag debug mode

--- src/singletons/drag-drop-manager.ts
+++ src/singletons/drag-drop-manager.ts
@@ -1,3 +1,14 @@
 import { DragDropManager } from "@dnd-kit/dom";
 
-export default new DragDropManager();
+import { defaultPreset } from "@dnd-kit/dom";
+import { Debug } from "@dnd-kit/dom/plugins/debug";
+
+const url = new URL(document.URL);
+
+const isDebugMode = url.searchParams.has("debug");
+
+export default new DragDropManager({
+  plugins: isDebugMode
+    ? [Debug, ...defaultPreset.plugins]
+    : defaultPreset.plugins,
+});
