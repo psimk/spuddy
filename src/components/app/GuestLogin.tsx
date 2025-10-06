b8815e00d3cfd8ab6001753689d32ef556267a4b
Paul <dev@psimk.dev>
2025-10-06 12:06:59 +0300

    remove custom network listener

--- src/components/app/GuestLogin.tsx
+++ src/components/app/GuestLogin.tsx
@@ -1,13 +1,10 @@
 import { useEffect } from "react";
 
 import db from "@services/instantdb/db";
-import networkListener from "@services/instantdb/network-listener";
 
 export default function GuestLogin() {
   useEffect(() => {
     db.auth.signInAsGuest();
-    // this doesn't run because the guest is already logged in
-    networkListener.forceOffline();
   }, []);
 
   return null;
--- src/services/instantdb/db.ts
+++ src/services/instantdb/db.ts
@@ -1,10 +1,9 @@
 import { init } from "@instantdb/react";
 
 import schema from "./schema";
-import networkListener from "./network-listener";
 
 export default init({
   appId: import.meta.env.PUBLIC_INSTANT_APP_ID,
   schema,
-  NetworkListener: networkListener,
+  devtool: false,
 });
