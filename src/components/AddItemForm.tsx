0328096e49e5efce87e23157d82b8fae740e50a0
Paul <dev@psimk.dev>
2026-02-07 14:52:25 +0200

    update add item form to match text area in size

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -8,7 +8,7 @@ export default function AddItemForm({ action, disabled }: Props) {
     <footer className="sticky bottom-0 p-4 pt-0">
       <form
         action={action}
-        className="rounded-box bg-base-100 p-2 drop-shadow-xl/30"
+        className="rounded-box bg-base-100 p-4 drop-shadow-xl/30"
       >
         <div className="flex">
           <input
@@ -16,7 +16,7 @@ export default function AddItemForm({ action, disabled }: Props) {
             name="text"
             disabled={disabled}
             placeholder="Add new item..."
-            className="input flex-1 rounded-xl input-ghost"
+            className="flex-1 outline-none h-6"
             autoFocus
           />
         </div>
